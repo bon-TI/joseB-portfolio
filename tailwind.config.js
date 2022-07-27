b2bcfa10a259a3f8bcd696dd47710fc20e3d542f
bon <joseipaz1203@gmail.com>
2022-07-27 12:20:08 -0700

    Setting Tailwindcss and importing react-icons

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,31 @@
 /** @type {import('tailwindcss').Config} */
+
 module.exports = {
-  content: [],
+  content: [
+    "./src/pages/**/*.{js,ts,jsx,tsx}",
+    "./src/components/**/*.{js,ts,jsx,tsx}"
+  ],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        inter: ["Inter", "sans-serif"],
+        lato: ["Lato", "sans-serif"],
+      },
+      colors: {
+        torch: {
+          50: "#FFB1C9",
+          100: "#FF76A0",
+          200: "#FF4E85",
+          300: "#FF276A",
+          400: "#FF145D",
+          500: "#FF004F",
+          600: "#EB0049",
+          700: "#D80043",
+          800: "#C4003D",
+          900: "#B10037",
+        }
+      }
+    },
   },
   plugins: [],
 }
